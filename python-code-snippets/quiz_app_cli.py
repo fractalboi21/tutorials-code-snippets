3fd5c03ebf9de373279ee865b8003e629d037828
aakashbilly <40062279+aakashbilly@users.noreply.github.com>
2021-06-15 22:59:46 +0530

    Update quiz_app_cli.py

--- python-code-snippets/quiz_app_cli.py
+++ python-code-snippets/quiz_app_cli.py
@@ -1,32 +1,10 @@
+#https://soshace.com/top-5-free-bootstrap-editors-tools/
+#https://graygrids.com/free-bootstrap-editors-builders/
 import json
 #load questions from json file
 data_json = open("questions.json","r").read()
 questions_list = json.loads(data_json)
 
-
-#iterate through question
-total_number_of_questions = len(questions_list)
-submitted_answers = []
-
-for i in range(total_number_of_questions):
-    print(f"question{[i+1]}")
-    question = questions_list[i]
-    print(question["question"])
-    for j in range(4):
-        choices = question["choices"]
-        print(f"{[j+1]} {question['choices'][j]}")
-    correct_ans = int(input("answer(int):"))
-    submitted_answers.append(correct_ans)
-
-print(submitted_answers)
-number_of_correct_answers = 0
-number_of_wrong_answers = 0
-for answer in questions_list:
-    
-    if int(answer["correct_ans"]) == int()
-
-
-    
 """
 command line quiz app exampe
 question[1]
@@ -41,4 +19,39 @@ answer:3
    ..........
    
 """
+
+
+#iterate through question
+total_number_of_questions = len(questions_list)
+submitted_answers = []
+
+for i in range(total_number_of_questions):
+    print("="*25)
+    print(f"question{[i+1]}")
+    question = questions_list[i]
+    print(question["question"])
+    for j in range(4):
+        choices = question["choices"]
+        print(f"{[j+1]} {question['choices'][j]}")
+    correct_ans = int(input("answer(int):"))
+    submitted_answers.append(correct_ans)
+
+total_number_of_submitted_answers = len(submitted_answers)
+number_of_correct_answers = 0
+number_of_wrong_answers = 0
+if total_number_of_questions == total_number_of_submitted_answers:
+    for i in range(total_number_of_questions):
+        if int(questions_list[i]["correct_ans"]) == int(submitted_answers[i]):
+            number_of_correct_answers += 1
+        else:
+            number_of_wrong_answers += 1
+
+print("number_of_correct_answers:",number_of_correct_answers)
+print("number_of_wrong_answers:",number_of_wrong_answers)        
+    
+
+    
+
+
+
 
