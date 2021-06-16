cd6f8352814b524d127692ae3313ad507c547ab6
aakashbilly <40062279+aakashbilly@users.noreply.github.com>
2021-06-16 21:59:07 +0530

    Create scrapeEverything.py

---
+++ python-code-snippets/scrapeEverything.py
@@ -0,0 +1,32 @@
+import requests
+from bs4 import BeautifulSoup
+html = requests.get("https://pythonawesome.com/").content
+soup = BeautifulSoup(html,"lxml")
+
+#tags link script a  noscript nav
+#href src
+def get_links_from_tag_attribute(tag,attribute):
+    script_tags = soup.find_all(tag)
+    src_links = []
+    for script_tag in script_tags:
+      try:
+        src = script_tag[attribute]
+        src_links.append(src)
+      except KeyError:
+        pass
+    return src_links
+
+def is_abs_link(link):
+    return link[0:4] == "http"
+
+def split_link_types(links):
+    abs_links = []
+    rel_links = []
+    for link in links:
+        if is_abs_link(link):
+            abs_links.append(link)
+        else:
+            rel_links.append(link)
+            
+    return abs_links, rel_links
+            
